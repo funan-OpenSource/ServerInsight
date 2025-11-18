c807dfd9dbce12157a530d9d79e97c5e0591da46
浮兰 <1455113971@qq.com>
2025-11-18 11:30:53 +0800

    初始化 2025-11-18

--- src/main/java/com/funan/serverinsight/ServerInsightAgentApplication.java
+++ src/main/java/com/funan/serverinsight/ServerInsightAgentApplication.java
@@ -3,6 +3,9 @@ package com.funan.serverinsight;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author funan
+ */
 @SpringBootApplication
 public class ServerInsightAgentApplication {
 
