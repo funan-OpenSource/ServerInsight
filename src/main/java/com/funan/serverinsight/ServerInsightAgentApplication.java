d10ceea083ad5e39191e3071472597609eb4f8f4
浮兰 <1455113971@qq.com>
2025-11-18 11:16:19 +0800

    初始化 2025-11-18

---
+++ src/main/java/com/funan/serverinsight/ServerInsightAgentApplication.java
@@ -0,0 +1,13 @@
+package com.funan.serverinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerInsightAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerInsightAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/funan/serverinsight/ServerInsightAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funan.serverinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerInsightAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
